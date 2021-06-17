3b788156d935c6a66ea6070fdfdccb6c24cf5a5f
yaozhoucn <wangxiao_h@126.com>
2021-06-17 16:43:37 +0800

    first

---
+++ src/main/java/com/yaozhou/pojo/BillPro.java
@@ -0,0 +1,14 @@
+package com.yaozhou.pojo;
+
+public class BillPro extends Bill {
+	private String proname;
+
+	public String getProname() {
+		return proname;
+	}
+
+	public void setProname(String proname) {
+		this.proname = proname;
+	}
+	
+}
--- src/main/java/com/yaozhou/pojo/User.java
+++ src/main/java/com/yaozhou/pojo/User.java
@@ -1,26 +1,146 @@
 package com.yaozhou.pojo;
 
-import java.math.BigInteger;
 import java.util.Date;
 
-/**
- * Created by WXHang on HANG at 2021/6/17 16:29
- * Desc：
- * @author
- */
 public class User {
-    private int id;
-    private String userCode;
-    private String userName;
-    private String userPassword;
-    private int gender;
-    private Date birthday;
-    private String phone;
-    private String address;
-    private int userRole;
-    private BigInteger createBy;
-    private Date creationDate;
-    private BigInteger modifyBy;
-    private Date modifyDate;
+    private Long id;
 
+    private String usercode;
+
+    private String username;
+
+    private String userpassword;
+
+    private Integer gender;
+    
+    //@DateTimeFormat(pattern="yyyy-MM-dd")
+    private Date birthday;
+
+    private String phone;
+
+    private String address;
+
+    private Integer userrole;
+
+    private Long createdby;
+
+    private Date creationdate;
+
+    private Long modifyby;
+
+    private Date modifydate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsercode() {
+        return usercode;
+    }
+
+    public void setUsercode(String usercode) {
+        this.usercode = usercode == null ? null : usercode.trim();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public String getUserpassword() {
+        return userpassword;
+    }
+
+    public void setUserpassword(String userpassword) {
+        this.userpassword = userpassword == null ? null : userpassword.trim();
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone == null ? null : phone.trim();
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address == null ? null : address.trim();
+    }
+
+    public Integer getUserrole() {
+        return userrole;
+    }
+
+    public void setUserrole(Integer userrole) {
+        this.userrole = userrole;
+    }
+
+    public Long getCreatedby() {
+        return createdby;
+    }
+
+    public void setCreatedby(Long createdby) {
+        this.createdby = createdby;
+    }
+
+    public Date getCreationdate() {
+        return creationdate;
+    }
+
+    public void setCreationdate(Date creationdate) {
+        this.creationdate = creationdate;
+    }
+
+    public Long getModifyby() {
+        return modifyby;
+    }
+
+    public void setModifyby(Long modifyby) {
+        this.modifyby = modifyby;
+    }
+
+    public Date getModifydate() {
+        return modifydate;
+    }
+
+    public void setModifydate(Date modifydate) {
+        this.modifydate = modifydate;
+    }
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", usercode=" + usercode + ", username="
+				+ username + ", userpassword=" + userpassword + ", gender="
+				+ gender + ", birthday=" + birthday + ", phone=" + phone
+				+ ", address=" + address + ", userrole=" + userrole
+				+ ", createdby=" + createdby + ", creationdate=" + creationdate
+				+ ", modifyby=" + modifyby + ", modifydate=" + modifydate + "]";
+	}
 }

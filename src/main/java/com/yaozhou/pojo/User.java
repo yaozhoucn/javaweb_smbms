7dd2830746e6580aa81f678b3d8448e39b6d5815
yaozhoucn <wangxiao_h@126.com>
2021-06-17 16:35:42 +0800

    first

---
+++ src/main/java/com/yaozhou/pojo/User.java
@@ -0,0 +1,26 @@
+package com.yaozhou.pojo;
+
+import java.math.BigInteger;
+import java.util.Date;
+
+/**
+ * Created by WXHang on HANG at 2021/6/17 16:29
+ * Desc：
+ * @author
+ */
+public class User {
+    private int id;
+    private String userCode;
+    private String userName;
+    private String userPassword;
+    private int gender;
+    private Date birthday;
+    private String phone;
+    private String address;
+    private int userRole;
+    private BigInteger createBy;
+    private Date creationDate;
+    private BigInteger modifyBy;
+    private Date modifyDate;
+
+}

8d04e7065042bb55b9cbd20ed35109b1dd719521
yaozhoucn <wangxiao_h@126.com>
2021-06-17 16:52:16 +0800

    first

---
+++ src/main/java/com/yaozhou/dao/BaseDao.java
@@ -0,0 +1,26 @@
+package com.yaozhou.dao;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Created by WXHang on HANG at 2021/6/17 16:43
+ * Desc：
+ * @author
+ */
+
+//操作数据库的公共类
+public class BaseDao {
+    private static String driver;
+    private static String url;
+    private static String username;
+    private static String password;
+
+    //静态代码块，在类加载的时候就初始化了
+    static {
+        Properties properties = new Properties();
+        //通过类加载器加载响应的资源
+        InputStream resource = BaseDao.class.getClassLoader().getResourceAsStream("db.properties");
+
+    }
+}

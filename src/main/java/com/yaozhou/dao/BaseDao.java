6c487759020cb1166b77d86870ee3cb02cd2054b
yaozhoucn <wangxiao_h@126.com>
2021-06-21 15:52:09 +0800

    first

--- src/main/java/com/yaozhou/dao/BaseDao.java
+++ src/main/java/com/yaozhou/dao/BaseDao.java
@@ -1,6 +1,8 @@
 package com.yaozhou.dao;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.sql.*;
 import java.util.Properties;
 
 /**
@@ -21,10 +23,119 @@ public class BaseDao {
         Properties properties = new Properties();
         //通过类加载器加载响应的资源
         InputStream resource = BaseDao.class.getClassLoader().getResourceAsStream("db.properties");
+        try {
+            properties.load(resource);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        driver = properties.getProperty("driver");
+        url = properties.getProperty("url");
+        username = properties.getProperty("username");
+        password = properties.getProperty("password");
 
 
     }
     //获取数据库的链接
+    public static Connection getConnection(String driver,String url,String username,String password){
+    //加载驱动
+        Connection connection = null;
+        try {
+            Class.forName(driver);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+
+    }
 
     //编写查询公共类
+    public static ResultSet executeQuery(Connection connection, String sql,PreparedStatement presm,ResultSet resultSet1,Object...params) throws SQLException {
+        ResultSet resultSet = null;
+
+        //预编译sql
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        for (int i = 0; i <params.length ; i++) {
+            preparedStatement.setObject(i+1,params[i]);
+        }
+
+        try {
+            resultSet = preparedStatement.executeQuery();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        }
+
+        return resultSet;
+
+    }
+
+    /**
+     * 编写增删改公共方法
+     * @return
+     */
+    public static int executeUpdate(Connection connection,String sql,PreparedStatement presm ,Object...params) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        int updateRows = 0;
+        for (int i = 0; i <params.length ; i++) {
+            preparedStatement.setObject(i+1,params[i]);
+        }
+        try {
+            updateRows = preparedStatement.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return updateRows;
+
+    }
+
+    /**
+     *
+     * @param connection
+     * @param resultSet
+     * @param preparedStatement
+     * @return
+     *
+     * 关闭资源，先判断是否为空，再关闭，如果出现异常，则认为资源关闭异常，返回flag = false；
+     */
+    public static boolean closeResource(Connection connection,ResultSet resultSet,PreparedStatement preparedStatement){
+        boolean flag = true;
+        if (resultSet != null){
+            try {
+                resultSet.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+                flag = false;
+            }
+            //GC回收
+            resultSet = null;
+        }
+        if (preparedStatement != null){
+            try {
+                preparedStatement.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+                flag = false;
+            }
+            //GC回收
+            preparedStatement = null;
+        }
+        if (connection != null){
+            try {
+                connection.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+                flag = false;
+            }
+            //GC回收
+            connection = null;
+        }
+        return  flag;
+
+    }
 }
---
+++ src/main/java/com/yaozhou/filter/CharacterEncodingFiler.java
@@ -0,0 +1,26 @@
+package com.yaozhou.filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * Created by WXHang on HANG at 2021/6/21 15:44
+ * Desc：
+ * @author HANG
+ */
+public class CharacterEncodingFiler implements Filter {
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        response.setContentType("text/html;charset=UTF-8");
+        request.setCharacterEncoding("utf-8");
+        response.setCharacterEncoding("utf-8");
+        chain.doFilter(request,response);
+    }
+
+    public void destroy() {
+
+    }
+}
